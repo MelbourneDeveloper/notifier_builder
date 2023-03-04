9530ece01aed3e17338e8d6f5e5d878fdd2caa7a
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-03-04 13:11:59 +1100

    Make this package more austere

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../example/lib/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
